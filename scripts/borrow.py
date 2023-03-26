d8d6c9d467b3f096f24776e3f6a4e2d901cdd7cb
ReasonZx <xico.reasonzx@gmail.com>
2023-03-26 23:36:18 +0200

    Getting Lending Pool address

--- scripts/borrow.py
+++ scripts/borrow.py
@@ -3,6 +3,22 @@ from scripts.getWeth import getWeth
 from brownie import config, network, interface
 
 
+def borrow(_account):
+    print("Naad")
+
+def getLendingPool():
+    lendingPoolAdressesProvider = interface.ILendingPoolAddressesProvider(config["networks"][network.show_active()]["lendingPoolAdressesProvider"])
+    lendingPoolAdress = lendingPoolAdressesProvider.getLendingPool()
+
+    lendingPool = interface.ILendingPool(lendingPoolAdress)
+    return lendingPool
+
 
 def main():
     account = getAccount()
+    if ("fork" in network.show_active()):
+        getWeth()
+    lendingPool = getLendingPool()
+    print(lendingPool)
+    
+    
