196150252c2e224a327bd59b0f72925197b24d10
ReasonZx <xico.reasonzx@gmail.com>
2023-06-08 16:11:50 +0200

    Borrow complete

--- scripts/borrow.py
+++ scripts/borrow.py
@@ -3,8 +3,26 @@ from scripts.getWeth import getWeth
 from brownie import config, network, interface
 from web3 import Web3
 
-def borrow(_account):
-    print("Naad")
+def depositERC20(lendingPool, ERC20adress, amount, account):
+    tx = lendingPool.deposit(ERC20adress, Web3.toWei(0.1, "ether"), account.address, 0, {"from" : account})
+    tx.wait(1)
+    print("Deposited!")
+    return tx
+
+def borrowERC20(lendingPool, tokenAdress, amount, borrowType, account):
+    tx = lendingPool.borrow(tokenAdress, amount, borrowType, 0 , account.address, {"from" : account})
+    tx.wait(1)
+    erc20 = interface.IERC20(tokenAdress)
+    print("Borrowed " + str(amount) + " " + erc20.symbol())
+    return tx
+
+def repayERC20(lendingPool, tokenAdress, amount, account):
+    if(amount == 'max'):
+        tx = lendingPool.repay(tokenAdress, type(uint256).max, borrowType, account.address, {"from" : account})
+        tx.wait(1)
+    else:
+        tx = lendingPool.repay(tokenAdress, amount, borrowType, account.address, {"from" : account})
+        tx.wait(1)
 
 def getLendingPool():
     lendingPoolAdressesProvider = interface.ILendingPoolAddressesProvider(config["networks"][network.show_active()]["lendingPoolAdressesProvider"])
@@ -29,6 +47,12 @@ def getAccBorrowData(lendingPool, account):
     print("Health factor: " + str(healthFactor))
     return (totalCollateralETH, totalDebtETH, str(availableBorrowsETH))
 
+def getAssetPrice(priceFeedAddress):
+    daiEthPriceFeed = interface.AggregatorV3Interface(priceFeedAddress)
+    latestPrice = daiEthPriceFeed.latestRoundData()[1]
+    latestPrice = Web3.fromWei(latestPrice, "ether")
+    print("The DAI/ETH price is "+ str(latestPrice))
+    return float(latestPrice)
 
 def main():
     account = getAccount()
@@ -39,12 +63,17 @@ def main():
 
     approveERC20(Web3.toWei(0.1, "ether"), lendingPool.address, wethAddress, account)
     
-    tx = lendingPool.deposit(wethAddress, Web3.toWei(0.1, "ether"), account.address, 0, {"from" : account})
-    tx.wait(1)
-    print("Deposited!")
-
+    depositERC20(lendingPool, wethAddress, Web3.toWei(0.1, "ether"), account)
 
     (totalCollateralETH, totalDebtETH, availableBorrowsETH) = getAccBorrowData(lendingPool, account)
+
+    daiEthPrice = getAssetPrice(config["networks"][network.show_active()]["daiEthPriceFeed"])
+
+    borrowERC20(lendingPool, config["networks"][network.show_active()]["daiToken"],  0.05/ daiEthPrice , 1, account)
+    
+    getAccBorrowData(lendingPool, account)
+
+    repayERC20(lendingPool, config["networks"][network.show_active()]["daiToken"], 'max', account)
 
 
     
